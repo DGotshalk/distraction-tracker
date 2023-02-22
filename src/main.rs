dec9f174e1119328014ec6f9abced2343a9d8062
Drew Gotshalk <32356221+DGotshalk@users.noreply.github.com>
2023-02-21 22:50:07 -1000

    removed the example/tutorial functions

--- src/main.rs
+++ src/main.rs
@@ -2,35 +2,22 @@
 // Copyright (C) 2023 dgotshalk <dgotshalk@Dissonance>
 // Distributed under terms of the MIT license.
 
-use actix_web::{ web, App, HttpServer };
-
+use actix_web::{App, HttpServer};
 
 mod routes;
-use crate::routes::{ AppState, hello, echo, manual_hello, index, homepage };
+use crate::routes::{homepage, iphistory};
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
     #[test]
     fn it_works() {}
 }
 
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .app_data(web::Data::new(AppState{
-                app_name: String::from("/")
-            }))
-            .service(homepage)
-            .service(hello)
-            .service(echo)
-            .route("/hey" , web::get().to(manual_hello))
-            .service(index)
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(homepage).service(iphistory))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
--- src/routes.rs
+++ src/routes.rs
@@ -4,35 +4,20 @@
 // Distributed under terms of the MIT license.
 //
 
-
-use actix_web::{ get, post, web, HttpResponse, Responder, HttpRequest };
-
-pub struct  AppState{
-    pub app_name: String
-}
+use actix_web::{get, HttpRequest, HttpResponse, Responder};
 
 #[get("/")]
 pub async fn homepage(req: HttpRequest) -> impl Responder {
-        let val = String::from(req.connection_info().realip_remote_addr().unwrap_or("None"));
-        HttpResponse::Ok().body(val)
+    //Now I need to get the string back from the request
+    let val = String::from(req.connection_info().realip_remote_addr().unwrap_or("None"));
+    // this is where I would pass this to a sql query handler
+    HttpResponse::Ok().body(val)
 }
 
-#[get("/hello")]
-pub async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-pub async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-pub async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
-
-#[get("/index")]
-pub async fn index(data: web::Data<AppState>) -> String {
-    let app_name = &data.app_name;
-    format!("Hello {app_name}")
+#[get("/history")]
+pub async fn iphistory(req: HttpRequest) -> impl Responder {
+    //Now I need to get the string back from the request
+    let val = String::from(req.connection_info().realip_remote_addr().unwrap_or("None"));
+    // this is where I would pass this to a sql query handler
+    HttpResponse::Ok().body(val)
 }
