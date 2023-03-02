6cd5b2f70b3a74c69ebadf87192702c6494e5c74
Drew Gotshalk <32356221+DGotshalk@users.noreply.github.com>
2023-03-02 00:03:55 -1000

    starting to serve static html files

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@
 use actix_web::{App, HttpServer};
 
 mod routes;
-use crate::routes::{homepage, iphistory};
+use crate::routes::{homepage, index, iphistory};
 
 #[cfg(test)]
 mod tests {
@@ -16,8 +16,13 @@ mod tests {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(homepage).service(iphistory))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(homepage)
+            .service(iphistory)
+            .service(index)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
--- src/routes.rs
+++ src/routes.rs
@@ -4,7 +4,9 @@
 // Distributed under terms of the MIT license.
 //
 
-use actix_web::{get, HttpRequest, HttpResponse, Responder};
+use actix_files::NamedFile;
+use actix_web::{get, HttpRequest, HttpResponse, Responder, Result};
+use std::path::PathBuf;
 
 #[get("/")]
 pub async fn homepage(req: HttpRequest) -> impl Responder {
@@ -20,4 +22,10 @@ pub async fn iphistory(req: HttpRequest) -> impl Responder {
     let val = String::from(req.connection_info().realip_remote_addr().unwrap_or("None"));
     // this is where I would pass this to a sql query handler
     HttpResponse::Ok().body(val)
+}
+
+#[get("/index")]
+pub async fn index(_req: HttpRequest) -> Result<NamedFile> {
+    let path: PathBuf = "./src/static/index.html".parse().unwrap();
+    Ok(NamedFile::open_async(path).await?)
 }
