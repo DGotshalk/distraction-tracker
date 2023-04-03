f83be2f0988cf99f6e0d553d07191f04847f92a1
Drew Gotshalk <32356221+DGotshalk@users.noreply.github.com>
2023-04-02 18:38:25 -1000

    finished moving over to axum

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,9 @@
 // Copyright (C) 2023 dgotshalk <dgotshalk@Dissonance>
 // Distributed under terms of the MIT license.
 
-use axum::{
-    http::StatusCode,
-    response::IntoResponse,
-    routing::{get, post},
-    Json, Router,
-};
+use axum::{routing::get, Router};
+use tower_http::services::ServeDir;
+
 mod routes;
 use routes::{homepage, iphistory};
 
@@ -22,8 +19,15 @@ mod tests {
 //may literally need the actix files
 #[tokio::main]
 async fn main() {
-    let app = Router::new();
+    let app = Router::new()
+        .route("/", get(homepage))
+        .route("/history", get(iphistory))
+        .nest_service(
+            "/static/assets/images",
+            ServeDir::new("static/assets/images"),
+        );
     let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 8080));
+
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
--- src/routes.rs
+++ src/routes.rs
@@ -4,17 +4,17 @@
 //
 
 use askama::Template;
-use axum::response::{Html, IntoResponse, Response};
-use serde::{Deserialize, Serialize};
-use serde_json::json;
+use axum::{
+    http::StatusCode,
+    response::{Html, IntoResponse},
+};
 
-#[derive(Serialize, Deserialize)]
 struct DateCount {
     date: String,
     count: i32,
 }
 
-pub async fn homepage() {
+pub async fn homepage() -> impl IntoResponse {
     // this is where I would pass this to a sql query handler
     // NEED:
     // date (we can generate)
@@ -22,14 +22,23 @@ pub async fn homepage() {
     // agent
     // generate counter from these counts and put them here :)
 
-    let data = json!({
-        "message": "Don't be distracted!",
-        "ip": "127.0.0.1",
-        "agent": "none"
-    });
+    let template = IndexTemplate {
+        message: String::from("Dont' be distracted"),
+        ip: String::from("127.0.0.1"),
+        agent: String::from("None"),
+    };
+
+    match template.render() {
+        Ok(html) => Html(html).into_response(),
+        Err(err) => (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Failed to render template. Error {}", err),
+        )
+            .into_response(),
+    }
 }
 
-pub async fn iphistory() {
+pub async fn iphistory() -> impl IntoResponse {
     //Now I need to get the string back from the request
     // this is where I would pass this to a sql query handler
     // NEED (would make a list of this data and put them in the dates env ):
@@ -38,37 +47,47 @@ pub async fn iphistory() {
     // agent
     // generate counter from these counts and put them here
 
-    let data = json!({
-        "message": "How often have you been distracted?",
-        "ip": "127.0.0.1",
-        "agent": "none",
-        "dates" : [
-            {
-                "date" : "10-03-2023",
-                "count": 1
-            },
-            {
-                "date" : "11-03-2023",
-                "count": 0
-            },
-            {
-                "date" : "12-03-2023",
-                "count": 2
-            },
-            {
-                "date" : "13-03-2023",
-                "count": 4
-            },
-            {
-                "date" : "14-03-2023",
-                "count": 3
-            },
-            {
-                "date" : "15-03-2023",
-                "count": 5
-            }
-        ]
-    });
+    let data: Vec<DateCount> = vec![
+        DateCount {
+            date: String::from("10-03-2023"),
+            count: 1,
+        },
+        DateCount {
+            date: String::from("11-03-2023"),
+            count: 0,
+        },
+        DateCount {
+            date: String::from("12-03-2023"),
+            count: 2,
+        },
+        DateCount {
+            date: String::from("13-03-2023"),
+            count: 4,
+        },
+        DateCount {
+            date: String::from("14-03-2023"),
+            count: 3,
+        },
+        DateCount {
+            date: String::from("15-03-2023"),
+            count: 5,
+        },
+    ];
+    let template = HistoryTemplate {
+        message: String::from("Dont' be distracted"),
+        ip: String::from("127.0.0.1"),
+        agent: String::from("None"),
+        dates: &data,
+    };
+
+    match template.render() {
+        Ok(html) => Html(html).into_response(),
+        Err(err) => (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Failed to render template. Error {}", err),
+        )
+            .into_response(),
+    }
 }
 
 #[derive(Template)]
@@ -77,4 +96,13 @@ struct IndexTemplate {
     message: String,
     ip: String,
     agent: String,
+}
+
+#[derive(Template)]
+#[template(path = "history.html")]
+struct HistoryTemplate<'a> {
+    message: String,
+    ip: String,
+    agent: String,
+    dates: &'a Vec<DateCount>,
 }
