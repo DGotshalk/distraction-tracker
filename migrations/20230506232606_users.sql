da850522c42e011d762bf913e499e78b517ef2a9
Drew Gotshalk <32356221+DGotshalk@users.noreply.github.com>
2023-05-06 22:59:26 -1000

    fixed dev.docker-compose.yml

--- migrations/20230506232606_users.sql
+++ migrations/20230506232606_users.sql
@@ -1,7 +1,5 @@
--- Add migration script here
 CREATE TABLE If NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    ip_address VARCHAR(45) NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL
+    ip_address VARCHAR(45) NOT NULL
 );
--- migrations/20230506232618_userconnections.sql
+++ migrations/20230506232618_userconnections.sql
@@ -1,1 +1,8 @@
--- Add migration script here
+CREATE TABLE IF NOT EXISTS user_connections (
+    id SERIAL PRIMARY KEY,
+    user_id BIGINT UNSIGNED,
+    connection_date DATE NOT NULL,
+    connection_count INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
