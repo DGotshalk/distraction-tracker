96b5448c6f5dcb7a6a57c6985124d41bd669f05f
Drew Gotshalk <32356221+DGotshalk@users.noreply.github.com>
2023-05-13 15:52:28 -1000

    adjusted migrations to fix issues and add unique constraints

--- migrations/20230506232606_users.sql
+++ migrations/20230506232606_users.sql
@@ -2,4 +2,5 @@ CREATE TABLE If NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     ip_address VARCHAR(45) NOT NULL,
 	user_agent VARCHAR(255) NOT NULL,
+	UNIQUE KEY (ip_address, user_agent)
 );
--- migrations/20230513233212_user_unique.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE users ADD CONSTRAINT UQ_Person UNIQUE KEY(ip_address, user_agent);
