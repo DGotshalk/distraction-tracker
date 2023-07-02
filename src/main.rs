a2772c22814fe7f3650ee25fd3ea3501ad14f85b
Drew Gotshalk <32356221+DGotshalk@users.noreply.github.com>
2023-07-01 22:03:16 -1000

    test

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use crate::db::connect_to_db;
 use crate::routes::{homepage::homepage, iphistory::iphistory};
 use axum::{routing::get, Extension, Router};
 use axum_client_ip::SecureClientIpSource;
+use sqlx::MySqlPool;
 use std::env;
 use tower_http::services::ServeDir;
 
@@ -21,18 +22,79 @@ async fn main() {
         connect_to_db(&env::var("DATABASE_URL").expect("Expected DATABASE_URL in environment"))
             .await;
 
-    let app = Router::new()
+    let app = app(pool).into_make_service_with_connect_info::<std::net::SocketAddr>();
+    let addr = std::net::SocketAddr::from(([0, 0, 0, 0], 8080));
+    axum::Server::bind(&addr).serve(app).await.unwrap();
+}
+
+fn app(pool: MySqlPool) -> Router {
+    Router::new()
         .route("/", get(homepage))
         .route("/history", get(iphistory))
         .nest_service("/images", ServeDir::new("./static/assets/images"))
         .layer(SecureClientIpSource::ConnectInfo.into_extension())
         .layer(Extension(pool))
-        .into_make_service_with_connect_info::<std::net::SocketAddr>();
-    let addr = std::net::SocketAddr::from(([0, 0, 0, 0], 8080));
-    axum::Server::bind(&addr).serve(app).await.unwrap();
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::db::connect_to_db;
+    use axum::http::{Request, StatusCode};
+    use std::env; //
+
+    #[tokio::test]
+    async fn test_routes() {
+        dotenv::dotenv().ok();
+        let pool =
+            connect_to_db(&env::var("DATABASE_URL").expect("Expected DATABASE_URL in environment"))
+                .await;
+        let listener =
+            std::net::TcpListener::bind("127.0.0.1:9090".parse::<std::net::SocketAddr>().unwrap())
+                .unwrap();
+        let app = app(pool).into_make_service_with_connect_info::<std::net::SocketAddr>();
+
+        tokio::spawn(async move {
+            axum::Server::from_tcp(listener)
+                .unwrap()
+                .serve(app)
+                .await
+                .unwrap()
+        });
+
+        let client = hyper::Client::new();
+        let response_index = client
+            .request(
+                Request::builder()
+                    .uri("http://127.0.0.1:9090/")
+                    .body(hyper::Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+        println!("{}", response_index.body());
+        assert_eq!(response_index.status(), StatusCode::OK);
+
+        let response_history = client
+            .request(
+                Request::builder()
+                    .uri("http://127.0.0.1:9090/history")
+                    .body(hyper::Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+        assert_eq!(response_history.status(), StatusCode::OK);
+
+        let response_image = client
+            .request(
+                Request::builder()
+                    .uri("http://127.0.0.1:9090/images/Quote.png")
+                    .body(hyper::Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+        assert_eq!(response_image.status(), StatusCode::OK);
+    }
 }
